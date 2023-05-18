8e3eb903b838b29f7372397a6a54e3417e9b1bff
Gavin Pardee <133939089+PoisonDart4341@users.noreply.github.com>
2023-05-17 23:15:15 -0500

    G64-P V 1.0

---
+++ G64-P.py
@@ -0,0 +1,40 @@
+def print_help():
+    print("Available")
+    print("help - Find usable commands")
+    print("version - Find software version")
+    print("about - About G64-P")
+    
+def print_version():
+    print("G64-P Version 1.0")
+    
+def print_about():
+    print("G(avin Pardee - Creator)(Based off of Commodore)64-P(ython Edition)")
+    
+def print_credits():
+    print("Coder - Gavin Pardee")
+    print("Programming Language - Python")
+    print("Programming Application - Thonny")
+    print("Special Thanks - Samuel Hicks")
+
+def process_command(command):
+    if command == "credits":
+        print_credits()
+    if command == "about":
+        print_about()
+    elif command == "version":
+        print_version()
+    elif command == "help":
+        print_help()
+    else:
+        print("Unknown command")
+        print("Please type 'help' for usable commands")
+        
+def main():
+    while True:
+        command = input("> ")
+        process_command(command)
+        
+if __name__ == "__main__":
+    print("******* G64-P *******")
+    print("Please type a command")
+    main()
